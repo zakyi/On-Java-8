ab5419849dc621faa586a86190a6cc9ed5487dec
zakyi <1062529808@qq.com>
2021-11-23 14:02:19 +0800

    first app

---
+++ src/HelloDate.java
@@ -0,0 +1,9 @@
+// ./src/HelloDate.java
+import java.util.*;
+
+class HelloDate {
+	public static void main(String[] args){
+		System.out.println("Hello, it's: ");
+		System.out.println(new Date());
+	}
+}
